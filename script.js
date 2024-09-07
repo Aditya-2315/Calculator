69b63a48dcd4493fda50543a8fbc5a69e8988364
Aditya Chaudhary <chaudharyaadityaa14@gmail.com>
2024-09-07 12:59:59 +0530

    Removed console log

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ let button = document.querySelector("#btn_container")
 
 button.addEventListener("click", e=>{
     if(e.target.classList.contains('num')){
-    //    console.log(e.target.innerHTML , equation.innerHTML.length)
+
        equation.innerHTML = equation.innerHTML + e.target.innerHTML
     }
     else if(e.target.classList.contains('operator')){
         equation.innerHTML = equation.innerHTML + e.target.innerHTML
     }
     else if(e.target.classList.contains('del')){
-        let eq = equation.innerHTML.slice(-1)
-        console.log(eq)
         equation.innerHTML = equation.innerHTML.slice(0,-1)
     }
     else if(e.target.classList.contains('equal')){
@@ -20,7 +18,6 @@ button.addEventListener("click", e=>{
         result.innerHTML = result.innerHTML + eval(equation.innerHTML)
     }
     else if(e.target.classList.contains('clear')){
-        console.log("clear")
         result.innerHTML = ""
         equation.innerHTML = ""
     }
